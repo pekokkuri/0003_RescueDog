f9e5060aa78ef0620cce41eddc731a0e03748ae6
peko <100201850+pekokkuri@users.noreply.github.com>
2025-06-04 02:27:33 +0900

    [add]投稿データとユーザーデータの紐づけ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -24,6 +25,10 @@ class PostController extends Controller
         $post->address = $request->address;
         $post->lat = $request->lat;
         $post->lng = $request->lng;
+
+        // ログインユーザーのIDをセット
+        $post->user_id = Auth::id();
+
         $post->save();
 
         return redirect()->route('posts.index');
