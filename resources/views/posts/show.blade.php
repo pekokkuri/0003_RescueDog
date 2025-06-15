121b8e3465bab7af5db706f9a11891b4219dbc4b
peko <100201850+pekokkuri@users.noreply.github.com>
2025-06-16 03:42:59 +0900

    [add]投稿の削除機能

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,15 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>show</title>
-</head>
-<body>
-  <a href="{{ route('posts.edit-post', ['post' => $post->id]) }}">投稿を編集する</a>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>show</title>
+  </head>
+  <body>
 
-  <p>{!! nl2br(e($post->address)) !!}</p>
+    <!-- 編集画面へ遷移 -->
+    <a href="{{ route('posts.edit-post', ['post' => $post->id]) }}">投稿を編集する</a>
 
-  <p><a href="{{ route('dashboard') }}">マイページへ戻る</a></p>
-</body>
+    <!-- 削除 -->
+    <form method="post" action="{{ route('posts.destroy', $post) }}" id="delete-form">
+      @method('DELETE')
+      @csrf
+      <button>投稿を削除する</button>
+    </form>
+
+    <p>{!! nl2br(e($post->address)) !!}</p>
+
+    <p><a href="{{ route('dashboard') }}">マイページへ戻る</a></p>
+  </body>
+
+  <script>
+    'use strict';
+
+      const form = document.querySelector('#delete-form');
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        if (confirm('本当に削除しますか?') === false) {
+              return;
+        }
+        form.submit();
+        });
+  </script>
 </html>
