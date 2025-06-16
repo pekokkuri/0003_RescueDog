7f4f61bf3679dd9ff4da5359038f2e534eb64e27
peko <100201850+pekokkuri@users.noreply.github.com>
2025-06-16 18:35:49 +0900

    [add]共通レイアウトファイルを作成

---
+++ resources/views/layouts/common.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ $title ?? 'Rescue Dog' }}</title>
+    <link rel="stylesheet" href="{{ url('style.css') }}" />
+</head>
+<body>
+    <header id="header" class="p-4 bg-gray-100">
+        <h1>迷子犬マップ</h1>
+        @if (Route::has('login'))
+        <nav>
+            @auth
+                <a href="{{ url('/dashboard') }}" class="mr-4">マイページ</a>
+            @else
+                <a href="{{ route('login') }}" class="mr-4">ログイン</a>
+                <a href="{{ route('register') }}">新規登録</a>
+            @endauth
+
+            @auth
+                <form method="POST" action="{{ route('logout') }}" class="inline">
+                    @csrf
+                    <button type="submit" class="ml-4">ログアウト</button>
+                </form>
+            @endauth
+        </nav>
+        @endif
+    </header>
+
+    <main class="p-6">
+        {{ $slot }}
+    </main>
+</body>
+</html>
