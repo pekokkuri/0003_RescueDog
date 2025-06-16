03bea7b13166874931b838ca416c2027b3ae8783
peko <100201850+pekokkuri@users.noreply.github.com>
2025-06-17 01:28:49 +0900

    [update]Tailwind CSS対応 -ver2

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'Rescue Dog')</title>
-    <link rel="stylesheet" href="{{ url('style.css') }}" />
+    <!-- <link rel="stylesheet" href="{{ url('style.css') }}" /> -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])  <!--Tailwind CSS対応-->
 </head>
 <body>
