e91b9ef4042358fd7d0f9e068e46d8f8d7628b42
peko <100201850+pekokkuri@users.noreply.github.com>
2025-06-16 18:44:11 +0900

    [edit]共通レイアウトファイルを修正

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>{{ $title ?? 'Rescue Dog' }}</title>
+    <title>@yield('title', 'Rescue Dog')</title>
     <link rel="stylesheet" href="{{ url('style.css') }}" />
 </head>
 <body>
@@ -29,7 +29,7 @@
     </header>
 
     <main class="p-6">
-        {{ $slot }}
+        @yield('content')
     </main>
 </body>
 </html>
