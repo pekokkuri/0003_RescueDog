f83346008115359267b1131cd81161c8e5331c7d
peko <100201850+pekokkuri@users.noreply.github.com>
2025-06-17 00:05:13 +0900

    [update]マイページに共通レイアウトを適用

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,8 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __("マイページ") }}
-        </h2>
-    </x-slot>
+@extends('layouts.common')
 
+@section('title', '投稿/RescueDog')
+
+@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -14,21 +12,20 @@
             </div>
         </div>
     </div>
-</x-app-layout>
 
-<div class="p-6 text-gray-900">
-    <p>現在の投稿一覧：</p>
+    <div class="p-6 text-gray-900">
+        <p>現在の投稿一覧：</p>
 
-    <ul>
-        @forelse ($posts as $post)
-            <li>
-                <a href="{{ route('posts.show', $post) }}" class="text-skyblue-100 underline hover:text-blue-100">
-                    {{ $post->address }}
-                </a>
-            </li>
-        @empty
-            <li>まだ投稿していません</li>
-        @endforelse
-
-    </ul>
-</div>
+        <ul>
+            @forelse ($posts as $post)
+                <li>
+                    <a href="{{ route('posts.show', $post) }}" class="text-skyblue-100 underline hover:text-blue-100">
+                        {{ $post->address }}
+                    </a>
+                </li>
+            @empty
+                <li>まだ投稿していません</li>
+            @endforelse
+        </ul>
+    </div>
+@endsection
