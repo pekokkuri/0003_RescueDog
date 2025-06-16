86152bffa40d549f44c9f7fa61fc38f20b12e9c3
peko <100201850+pekokkuri@users.noreply.github.com>
2025-06-16 18:49:07 +0900

    [update]トップ画面に共通レイアウトを適用

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,56 +1,24 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="{{ url('style.css') }}" />
-        <title>Rescue Dog</title>
-    </head>
-    <body>
-        <header id="header">
-        <h1>迷子犬マップ</h1>
-            @if (Route::has('login'))
-            <nav>
-                @auth
-                    <a href="{{ url('/dashboard') }}">マイページ</a>
-                @else
-                    <a href="{{ route('login') }}">ログイン</a>
-                    <a href="{{ route('register') }}">新規登録</a>
-                @endauth
+@extends('layouts.common')
 
-                @auth
-                    <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                    <button type="submit">ログアウト</button>
-                    </form>
-                @endauth
-            </nav>
-            @endif
-        </header>
+@section('title', 'Rescue Dog マップ')
 
-        <div id="map"></div>
+@section('content')
+    <div id="map"></div>
 
-        <script>
-            // コントローラで取得したデータ($posts)をJavascriptでも扱えるようにJSON形式で定義
-            const posts = @json($posts);
-        </script>
-        <script src="{{ url('/js/map.js') }}"></script>
-        <script
-            src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{
-                config('services.google_maps.api_key')
-            }}&callback=initMap"
-            async
-            defer
-        ></script>
-        
-        @if(session('error'))
-        <script>
-            alert("{{ session('error') }}");
-        </script>
-        @endif
-        
-        <a href="{{ route('posts.create') }}">
-            <button>投稿する</button>
-        </a>
-    </body>
-</html>
+    <script>
+        const posts = @json($posts);
+    </script>
+    <script src="{{ url('/js/map.js') }}"></script>
+    <script
+        src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{ config('services.google_maps.api_key') }}&callback=initMap"
+        async defer
+    ></script>
+
+    @if(session('error'))
+        <script>alert("{{ session('error') }}");</script>
+    @endif
+
+    <a href="{{ route('posts.create') }}">
+        <button>投稿する</button>
+    </a>
+@endsection
