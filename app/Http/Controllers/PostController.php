2b3552d08f9125e14784a1814d3a94566a20055a
peko <100201850+pekokkuri@users.noreply.github.com>
2025-07-01 00:03:02 +0900

    [revert] PostController.php を e5f1ebc の状態に戻す

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,13 +31,10 @@ class PostController extends Controller
     {
         $post = new Post();
 
-        if ($request->hasFile('image') && $request->file('image')->isValid()) {
-            $path = $request->file('image')->store('images', 'public');
-            $post->image_path = $path;
-        } else {
-            $post->image_path = null;
-        }
+        // 投稿画像をstorage/imagesに保存（自動でユニークな名前を割り当てる）
+        $path = $request->file('image')->store('images', 'public');
 
+        $post->image_path = $path;
         $post->address = $request->address;
         $post->lat = $request->lat;
         $post->lng = $request->lng;
