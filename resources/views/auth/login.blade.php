dd7e4d0b5bb9f4b02df47d92bd16deb2393e151f
peko <100201850+pekokkuri@users.noreply.github.com>
2025-07-06 22:57:02 +0900

    [syle]ログインページ：共通レイアウトを適用

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,8 @@
-<x-guest-layout>
+@extends('layouts.common')
+
+@section('title', 'ログイン/RescueDog')
+
+@section('content')
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -48,4 +52,4 @@
     <div class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
         <a href="{{ route('posts.index') }}">戻る</a>
     </div>
-</x-guest-layout>
+@endsection
