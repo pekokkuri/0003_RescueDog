e7fd39b73d244a4fcce838f0735494504e03cdbd
peko <100201850+pekokkuri@users.noreply.github.com>
2025-07-07 22:56:52 +0900

    [style]詳細ページ：編集ボタンと削除ボタンのレイアウト調整

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,16 +4,21 @@
 
 @section('content')
 
+<div class="flex gap-4">
     <!-- 編集画面へ遷移 -->
-    <a href="{{ route('posts.edit-post', ['post' => $post->id]) }}">投稿を編集する</a>
+    <a href="{{ route('posts.edit-post', ['post' => $post->id]) }}" class="bg-blue-800 hover:bg-blue-700 text-white text-center rounded px-4 py-2">
+      投稿を編集する
+    </a>
 
     <!-- 削除 -->
     <form method="post" action="{{ route('posts.destroy', $post) }}" id="delete-form">
       @method('DELETE')
       @csrf
-      <button>投稿を削除する</button>
+      <button class="bg-red-800 hover:bg-red-700 text-white text-center rounded px-4 py-2">
+        投稿を削除する
+      </button>
     </form>
-
+</div>
     <!-- 投稿画像を表示 -->
     @if ($post->image_path)
       <img src="{{ asset('storage/' . $post->image_path) }}" alt="投稿画像" class="h-[300px] w-[300px]">
