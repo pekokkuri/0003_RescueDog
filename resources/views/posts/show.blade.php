d3f920d57979cf7a4ac1edc633c1fd1786ad4d27
peko <100201850+pekokkuri@users.noreply.github.com>
2025-07-08 20:51:08 +0900

    [style]詳細ページ：レイアウト調整

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,13 @@
 
 @section('content')
 
-<div class="flex gap-4">
+<div class="border border-gray-400 rounded p-4 m-4">
+
+  <!-----------------
+    ボタン
+  ------------------->
+  <div class="flex gap-4 justify-end">
+
     <!-- 編集画面へ遷移 -->
     <a href="{{ route('posts.edit-post', ['post' => $post->id]) }}" class="bg-blue-800 hover:bg-blue-700 text-white text-center rounded px-4 py-2">
       投稿を編集する
@@ -18,24 +24,29 @@
         投稿を削除する
       </button>
     </form>
-</div>
+  </div>
 
-<div class="flex m-8">
-    <!-- 投稿画像を表示 -->
-    @if ($post->image_path)
-      <img src="{{ asset('storage/' . $post->image_path) }}" alt="投稿画像" class="h-[300px] w-[300px]">
-    @else
-      <p>投稿した画像はありません</p>
-    @endif
-
+  <!-----------------
+      投稿内容を表示
+  ------------------->
+  <div class="flex m-8">
     
-    <div class="ml-6">
-      <label>
-        場所：{!! nl2br(e(Str::contains($post->address, '付近') ? $post->address : $post->address . '付近')) !!}
-      </label>
-    </div>
+      <!-- 投稿画像を表示 -->
+      @if ($post->image_path)
+        <img src="{{ asset('storage/' . $post->image_path) }}" alt="投稿画像" class="h-[300px] w-[300px]">
+      @else
+        <p>投稿した画像はありません</p>
+      @endif
 
-    <!-- <p><a href="{{ route('dashboard') }}">マイページへ戻る</a></p> -->
+      
+      <div class="ml-6">
+        <label>
+          場所：{!! nl2br(e(Str::contains($post->address, '付近') ? $post->address : $post->address . '付近')) !!}
+        </label>
+      </div>
+
+      <!-- <p><a href="{{ route('dashboard') }}">マイページへ戻る</a></p> -->
+  </div>
 </div>
 
   <script>
