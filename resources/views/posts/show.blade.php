456ee694f957dd3ddcc4c65f51d74775a2ce4863
peko <100201850+pekokkuri@users.noreply.github.com>
2025-07-13 22:36:39 +0900

    [style]詳細ページ：レイアウト調整

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,61 +4,64 @@
 
 @section('content')
 
-<div class="border border-gray-400 rounded p-4 m-4">
+<div class="flex justify-center mt-4">
+  <div class="border border-gray-400 rounded p-4 m-4 w-[1000px] h-dvh">
+    <!-----------------
+      マップ表示
+    ------------------->
+    <div id="map" class="h-[250px] mt-4"></div>
 
-  <div id="map" class="h-[250px] mt-4"></div>
+    <script>
+      const post = @json($post);
+    </script>
+    <script src="{{ url('/js/show.map.js') }}"></script>
+    <script
+      src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{ config('services.google_maps.api_key') }}&callback=initMap"
+      async defer
+    ></script>
 
-  <script>
-    const post = @json($post);
-  </script>
-  <script src="{{ url('/js/show.map.js') }}"></script>
-  <script
-    src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key={{ config('services.google_maps.api_key') }}&callback=initMap"
-    async defer
-  ></script>
+    <!-----------------
+      ボタン
+    ------------------->
+    <div class="flex gap-4 justify-end pt-4">
 
-  <!-----------------
-    ボタン
-  ------------------->
-  <div class="flex gap-4 justify-end pt-4">
+      <!-- 編集画面へ遷移 -->
+      <a href="{{ route('posts.edit-post', ['post' => $post->id]) }}" class="bg-blue-800 hover:bg-blue-700 text-white text-center rounded px-4 py-2">
+        投稿を編集する
+      </a>
 
-    <!-- 編集画面へ遷移 -->
-    <a href="{{ route('posts.edit-post', ['post' => $post->id]) }}" class="bg-blue-800 hover:bg-blue-700 text-white text-center rounded px-4 py-2">
-      投稿を編集する
-    </a>
+      <!-- 削除 -->
+      <form method="post" action="{{ route('posts.destroy', $post) }}" id="delete-form">
+        @method('DELETE')
+        @csrf
+        <button class="bg-red-800 hover:bg-red-700 text-white text-center rounded px-4 py-2">
+          投稿を削除する
+        </button>
+      </form>
+    </div>
 
-    <!-- 削除 -->
-    <form method="post" action="{{ route('posts.destroy', $post) }}" id="delete-form">
-      @method('DELETE')
-      @csrf
-      <button class="bg-red-800 hover:bg-red-700 text-white text-center rounded px-4 py-2">
-        投稿を削除する
-      </button>
-    </form>
-  </div>
+    <!-----------------
+        投稿内容を表示
+    ------------------->
+    <div class="flex m-8">
+      
+        <!-- 投稿画像を表示 -->
+        @if ($post->image_path)
+          <img src="{{ asset('storage/' . $post->image_path) }}" alt="投稿画像" class="h-[300px] w-[300px]">
+        @else
+          <img src="/images/NoImage.png" alt="投稿された画像はありません" class="h-[300px] w-[300px]">
+        @endif
 
-  <!-----------------
-      投稿内容を表示
-  ------------------->
-  <div class="flex m-8">
-    
-      <!-- 投稿画像を表示 -->
-      @if ($post->image_path)
-        <img src="{{ asset('storage/' . $post->image_path) }}" alt="投稿画像" class="h-[300px] w-[300px]">
-      @else
-        <img src="/images/NoImage.png" alt="投稿された画像はありません" class="h-[300px] w-[300px]">
-      @endif
+        <div class="ml-6">
+          <label>
+            場所：{!! nl2br(e(Str::contains($post->address, '付近') ? $post->address : $post->address . '付近')) !!}
+          </label>
+        </div>
 
-      <div class="ml-6">
-        <label>
-          場所：{!! nl2br(e(Str::contains($post->address, '付近') ? $post->address : $post->address . '付近')) !!}
-        </label>
-      </div>
-
-      <!-- <p><a href="{{ route('dashboard') }}">マイページへ戻る</a></p> -->
-  </div>
+        <!-- <p><a href="{{ route('dashboard') }}">マイページへ戻る</a></p> -->
+    </div>
 </div>
-
+</div>
   <script>
     'use strict';
 
