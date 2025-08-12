66c0da50b4ac1ae498ec2eb87debaceea5886648
peko <100201850+pekokkuri@users.noreply.github.com>
2025-08-12 18:17:25 +0900

    [add]通知コントローラー：通知既読後の処理を追加

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Notification;
+
+class NotificationController extends Controller
+{
+  public function is_read(Notification $notification) {
+    $notification->is_read = true;
+    $notification->save();
+
+    if ($notification->type === 'comment' && $notification->comment) {
+      return redirect()->route('posts.show', $notification->comment->post_id);
+    } elseif ($notification->type === 'reply' && $notification->reply) {
+      return redirect()->route('posts.show', $notification->reply->comment->post_id);
+    }
+  }
+}  
