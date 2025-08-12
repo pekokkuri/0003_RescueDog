9b38f389a046e5a7856931033ba238912c4a18bf
peko <100201850+pekokkuri@users.noreply.github.com>
2025-08-12 21:04:26 +0900

    [update]フラッシュメッセージ：progressBarの無効化＆ログアウト後にメッセージ表示

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -47,7 +47,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/')
+        ->with('flashSuccess', "ログアウトしました");;
     }
 
     // ログイン成功後、マイページを表示
