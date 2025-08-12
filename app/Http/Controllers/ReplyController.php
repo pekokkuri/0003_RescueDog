78f10707ec865927051bd8f0583c213e3e06b75d
peko <100201850+pekokkuri@users.noreply.github.com>
2025-08-12 16:21:46 +0900

    [update]Replyコントローラー：通知発火処理を追加

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Comment;
 use App\Models\Reply;
+use App\Models\Notification;
 
 class ReplyController extends Controller
 {
-    public function store(Request $request, Comment $comment)
+    public function reply_store(Request $request, Comment $comment)
     {
         $reply = new Reply();
         $reply->comment_id = $comment->id;
@@ -16,6 +17,24 @@ class ReplyController extends Controller
         $reply->body = $request->body;
         $reply->save();
 
+        $this->reply_notice_store($reply); // 通知作成を呼び出し
+
         return back()->with('flashSuccess', 'コメントに返信しました');
     }
+
+    private function reply_notice_store(Reply $reply)
+    {
+        // 自分のコメントに自分が返信した場合は通知しない
+        if ($reply->comment->user_id === $reply->user_id) {
+            return;
+        }
+
+        $notification = new Notification();
+        $notification->user_id = $reply->comment->user_id; // コメント者へ通知
+        $notification->type = 'reply';
+        $notification->reply_id = $reply->id;
+        $notification->is_read = false;
+        $notification->save();
+    }
 }
+
